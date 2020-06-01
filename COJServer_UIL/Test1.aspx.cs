7b7fd57ae8e9c42c8f696b179e6e8b4f695d3904
FREEstriker <2317416569@qq.com>
2020-06-02 01:53:14 +0800

    web服务器

---
+++ COJServer_UIL/Test1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using CalculateOnlineJudge.BusinessLogic_BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COJServer_UIL
+{
+    public partial class Test1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string userName = "FREEstrikerABbos";
+            string password = "1236zccg99326TEST";
+            var OR = UserLogic.CreateUser(userName, password);
+            Label1.Text = OR.Prompt;
+        }
+    }
+}
